fedaec194ef18db383cc41323e3313fb6b9f1520
arfan36 <arfanur36@gmail.com>
2022-08-21 12:54:24 +0600

    27-5 (semi-advanced) Common function to

---
+++ js/5_deposit.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-deposit').onclick=function(){
+    /* 
+    1. get the element ty id
+    2. get the value from the element
+    3. convert the string to a number
+     */
+}
