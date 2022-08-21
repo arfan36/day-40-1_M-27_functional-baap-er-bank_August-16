794ce674e2e9d10dd85f82a324d2d889ccd7e7c5
arfan36 <arfanur36@gmail.com>
2022-08-21 13:23:22 +0600

    27-6 (advanced) Share one function

--- js/5_deposit.js
+++ js/5_deposit.js
@@ -1,7 +1,23 @@
-document.getElementById('btn-deposit').onclick=function(){
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
+}
+
+document.getElementById('btn-deposit').onclick = function () {
     /* 
     1. get the element ty id
     2. get the value from the element
     3. convert the string to a number
      */
-}
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+};
